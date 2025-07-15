569b8a0af2df775e92d5ebe44a8d968c5536d8f0
janika nano <janano@taltech.ee>
2025-07-15 15:42:14 +0300

    InsuranceAPI: add initial Spring Initializr

---
+++ src/test/java/com/nano/insuranceapi/InsuranceapiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nano.insuranceapi;
+
+import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class InsuranceapiApplicationTests {
+
+	@Test
+	void test1() {
+		// InsuranceapiApplication.main(new String[] {});
+		System.out.println("Insurance API Application Test");
+	}
+
+}
