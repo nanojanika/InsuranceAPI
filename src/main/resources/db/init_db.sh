02e35a5fba7d1024f8590cbac8c5df2441e162e8
janika nano <janano@taltech.ee>
2025-07-16 20:25:16 +0300

    InsuranceAPI: fix application.yml 4 docker compose; remove extra testdata inserts

--- src/main/resources/db/init_db.sh
+++ src/main/resources/db/init_db.sh
@@ -9,7 +9,4 @@ sleep 60
 echo "Running init.sql..."
 sqlplus system/MyStrongPassword123@oracle-xe-composed:1521/XEPDB1 @/dbscripts/init.sql
 
-#echo "Running add_package.sql..."
-#sqlplus system/MyStrongPassword123@oracle-xe-composed:1521/XEPDB1 @/dbscripts/add_package.sql
-
 echo "Database initialization complete."
