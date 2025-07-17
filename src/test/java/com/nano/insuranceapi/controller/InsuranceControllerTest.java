229a9ab8ada6a9fcfaa243edbad4198cd3d276d8
janika nano <janano@taltech.ee>
2025-07-17 20:31:21 +0300

    InsuranceAPI: replace deprecated @MockBean --> @MockitoBean

--- src/test/java/com/nano/insuranceapi/controller/InsuranceControllerTest.java
+++ src/test/java/com/nano/insuranceapi/controller/InsuranceControllerTest.java
@@ -11,7 +11,7 @@ import com.nano.insuranceapi.service.InsuranceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,7 +33,7 @@ public class InsuranceControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private InsuranceService service;
 
     @Test
