64980c303fc235bc1e1f877acb5f626dcd909c10
janika nano <janano@taltech.ee>
2025-07-17 20:57:52 +0300

    InsuranceAPI: change test1 --> loadApplicationContext test

--- src/test/java/com/nano/insuranceapi/InsuranceapiApplicationTests.java
+++ src/test/java/com/nano/insuranceapi/InsuranceapiApplicationTests.java
@@ -1,15 +1,25 @@
 package com.nano.insuranceapi;
 
+import com.nano.insuranceapi.repository.InsuranceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:oracle:thin:@localhost:1521/XEPDB1",
+    "spring.datasource.driver-class-name=oracle.jdbc.OracleDriver",
+    "spring.datasource.username=fake_user",
+    "spring.datasource.password=fake_password",
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class InsuranceapiApplicationTests {
 
-	@Test
-	void test1() {
-		InsuranceapiApplication.main(new String[] {});
-		System.out.println("Insurance API Application Test");
-	}
+    @MockitoBean
+    private InsuranceRepository insuranceRepository;
+
+    @Test
+    void loadApplicationContext() {
+        System.out.println("Insurance API Application Test - Context Loaded Successfully");
+    }
 
 }
